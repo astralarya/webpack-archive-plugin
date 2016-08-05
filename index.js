9eab2acdd9b893457d2d72f079b974e049324a57
Mara Kim <mara@autochthe.net>
2016-08-05 01:41:07 -0500

    Archive both types

--- index.js
+++ index.js
@@ -9,15 +9,26 @@ WebpackArchivePlugin.prototype.apply = function(compiler) {
 	compiler.plugin("after-emit", function(compiler, callback) {
 		let output = compiler.options.output.path;
 
-		// Create tarfile
-		let tar = archiver('tar');
+		// Create archivers
+		let zip = archiver('zip');
+		zip.pipe(fs.createWriteStream(`${output}.zip`));
+		let tar = archiver('tar', {
+			gzip: true,
+			gzipOptions: {
+				level: 1
+			}
+		});
 		tar.pipe(fs.createWriteStream(`${output}.tar.gz`));
 
+		//
 		for(let asset in compiler.assets) {
-			console.log(asset);
-			tar.append(fs.createReadStream(file1), {name: 'file1.txt'});
+			if(compiler.assets.hasOwnProperty(asset)) {
+				zip.append(fs.createReadStream(compiler.assets[asset].existsAt), {name: asset});
+				tar.append(fs.createReadStream(compiler.assets[asset].existsAt), {name: asset});
+			}
 		}
 
+		zip.finalize();
 		tar.finalize();
 
 		callback();
