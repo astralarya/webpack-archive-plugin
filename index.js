07bf347486b995afb6bc1fa3f31a0274ec757abd
Mara Kim <mara@autochthe.net>
2016-08-15 15:01:34 -0500

    Add format switches

--- index.js
+++ index.js
@@ -15,27 +15,48 @@ WebpackArchivePlugin.prototype.apply = function(compiler) {
 		const output = options.output?
 			options.output:compiler.options.output.path;
 
-		// Create archivers
-		let zip = archiver('zip');
-		zip.pipe(fs.createWriteStream(`${output}.zip`));
-		let tar = archiver('tar', {
-			gzip: true,
-			gzipOptions: {
-				level: 1
+		// Create archive streams
+		let streams = [];
+		let zip = true;
+		let tar = true;
+		if(options.format) {
+			if(typeof options.format === 'string') {
+				zip = (options.format === 'zip');
+				tar = (options.format === 'tar');
+			} else if(Array.isArray(options.format)) {
+				zip = (options.format.indexOf('zip') != -1);
+				tar = (options.format.indexOf('tar') != -1);
 			}
-		});
-		tar.pipe(fs.createWriteStream(`${output}.tar.gz`));
+		}
+		if(zip) {
+			let stream = archiver('zip');
+			stream.pipe(fs.createWriteStream(`${output}.zip`));
+			streams.push(stream);
+		}
+		if(tar) {
+			let stream = archiver('tar', {
+				gzip: true,
+				gzipOptions: {
+					level: 1
+				}
+			});
+			stream.pipe(fs.createWriteStream(`${output}.tar.gz`));
+			streams.push(stream);
+		}
 
 		// Add assets
 		for(let asset in compiler.assets) {
 			if(compiler.assets.hasOwnProperty(asset)) {
-				zip.append(fs.createReadStream(compiler.assets[asset].existsAt), {name: asset});
-				tar.append(fs.createReadStream(compiler.assets[asset].existsAt), {name: asset});
+				for(let stream of streams) {
+					stream.append(fs.createReadStream(compiler.assets[asset].existsAt), {name: asset});
+				}
 			}
 		}
 
-		zip.finalize();
-		tar.finalize();
+		// Finalize streams
+		for(let stream of streams) {
+			stream.finalize();
+		}
 
 		callback();
 	});
